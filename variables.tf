d76e90274f4966285fb8191f004ceee40acf531e
thorvald <thor.gudj@gmail.com>
2026-06-19 19:39:20 +0200

    Added subnets for networking

--- variables.tf
+++ variables.tf
@@ -9,3 +9,21 @@ variable "networking_resource_group_name"{
     type        = string
     default     = "rg-networking"
 }
+
+variable "app_subnet_group_name"{
+    description = "App subnet"
+    type        = string
+    default     = "snet-app"
+}
+
+variable "Data_subnet_group_name"{
+    description = "Data subnet"
+    type        = string
+    default     = "snet-data"
+}
+
+variable "Management_subnet_group_name"{
+    description = "Management subnet"
+    type        = string
+    default     = "snet-mgmt"
+}
