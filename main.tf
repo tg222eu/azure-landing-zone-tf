9a628afdfb91fb2974de62ee0fd39e73017f8c9d
thorvald <thor.gudj@gmail.com>
2026-06-20 23:17:10 +0200

    Remove implicit dependency, add Depend_on

--- main.tf
+++ main.tf
@@ -15,9 +15,9 @@ resource "azurerm_resource_group" "platform" {
 resource "azurerm_virtual_network" "hub" {
   name                 = var.virtual_network_name
   location             = var.location
-  resource_group_name  = azurerm_resource_group.platform.name # Creates implicit dependency
+  resource_group_name  = azurerm_resource_group.platform.name
   address_space        = ["10.0.0.0/16"]
-  depends_on           = [var.platform_resource_group_name]
+  depends_on = [azurerm_virtual_network.hub]
 }
 
 resource "azurerm_subnet" "app" {
